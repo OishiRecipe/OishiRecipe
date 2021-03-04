9fcdc4d17bf19fe48500f0e6f651652db303a2a8
TolgaKara <tolgakara2.0@gmail.com>
2021-03-04 09:59:54 +0100

    Added CTA Button

--- pages/Landingpage/Landingpage.jsx
+++ pages/Landingpage/Landingpage.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import styles from "./Landingpage.module.css";
 
 const Landingpage = () => {
-	return <div>Landingpage</div>;
+	return (
+		<div>
+			<div class='hero'>
+				<div className='cta'>
+					<div className='cta-text'></div>
+					<button className={styles.ctaButton}>Ready to cook</button>
+				</div>
+			</div>
+			<div className='features'></div>
+		</div>
+	);
 };
 
 export default Landingpage;
