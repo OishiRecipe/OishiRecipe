c29782bae3340d7949ba73e9ffb5e81d8092bff2
TolgaKara <tolgakara2.0@gmail.com>
2021-03-04 21:15:43 +0100

    Cleaned up

--- pages/Landingpage/Landingpage.jsx
+++ pages/Landingpage/Landingpage.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import styles from "./Landingpage.module.css";
-import heroImg from "../../images/heroImg.png";
 
 const Landingpage = () => {
 	return (
 		<div>
 			<div class={styles.hero}>
 				<div className='cta'>
-					<img src={heroImg} alt='' />
 					<div className='cta-text'>
 						You wanna explore the culinary world and wanna follow a step by step guide on how to
 						cook it. Then theres is no need to look further just head in and share your culinary
