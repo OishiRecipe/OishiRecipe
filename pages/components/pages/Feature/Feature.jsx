d5d4eee77f1878e23acf218ff156fd62272c3f40
TolgaKara <tolgakara2.0@gmail.com>
2021-03-11 18:32:38 +0100

    Added Features

---
+++ pages/components/pages/Feature/Feature.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export const Feature = () => {
+	return (
+		<div>
+			<div className='feature-card'>
+				<h2 className='feature-title'>Share your recepies</h2>
+			</div>
+			<div className='feature-card'>
+				<h2 className='feature-title'>Get Feedback from the community</h2>
+			</div>
+			<div className='feature-card'>
+				<h2 className='feature-title'>Build your Networks of cookers</h2>
+			</div>
+			<div className='feature-card'>
+				<h2 className='feature-title'>Explore different cultural recepies</h2>
+			</div>
+			<div className='feature-card'>
+				<h2 className='feature-title'>Just enjoy eating</h2>
+			</div>
+		</div>
+	);
+};
