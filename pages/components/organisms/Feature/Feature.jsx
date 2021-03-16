f3a6ba60e09705a8169348538a36cee3f63d1fe7
TolgaKara <tolgakara2.0@gmail.com>
2021-03-16 12:50:50 +0100

    Added lorem Text feature_description

--- pages/components/organisms/Feature/Feature.jsx
+++ pages/components/organisms/Feature/Feature.jsx
@@ -4,19 +4,49 @@ export const Feature = () => {
 	return (
 		<div>
 			<div className='feature-card'>
+				<img src='' alt='feature img that showcase share your recepies' />
 				<h2 className='feature-title'>Share your recepies</h2>
+				<p className='feature-description'>
+					Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae corrupti cupiditate
+					blanditiis provident rerum veniam sed obcaecati alias, culpa enim non necessitatibus
+					laboriosam esse laudantium, tempore asperiores nesciunt! Totam, molestias.
+				</p>
 			</div>
 			<div className='feature-card'>
+				<img src='' alt='feature img that showcase Get Feedback from the community' />
 				<h2 className='feature-title'>Get Feedback from the community</h2>
+				<p className='feature-description'>
+					Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae corrupti cupiditate
+					blanditiis provident rerum veniam sed obcaecati alias, culpa enim non necessitatibus
+					laboriosam esse laudantium, tempore asperiores nesciunt! Totam, molestias.
+				</p>
 			</div>
 			<div className='feature-card'>
+				<img src='' alt='feature img that showcase Build your Networks of cookers' />
 				<h2 className='feature-title'>Build your Networks of cookers</h2>
+				<p className='feature-description'>
+					Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae corrupti cupiditate
+					blanditiis provident rerum veniam sed obcaecati alias, culpa enim non necessitatibus
+					laboriosam esse laudantium, tempore asperiores nesciunt! Totam, molestias.
+				</p>
 			</div>
 			<div className='feature-card'>
+				<img src='' alt='feature img that showcase Explore different cultural recepies' />
 				<h2 className='feature-title'>Explore different cultural recepies</h2>
+				<p className='feature-description'>
+					Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae corrupti cupiditate
+					blanditiis provident rerum veniam sed obcaecati alias, culpa enim non necessitatibus
+					laboriosam esse laudantium, tempore asperiores nesciunt! Totam, molestias.
+				</p>
 			</div>
 			<div className='feature-card'>
+				<img src='' alt='Just enjoy eating' />
 				<h2 className='feature-title'>Just enjoy eating</h2>
+				<p className='feature-description'>
+					Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae corrupti cupiditate
+					blanditiis provident rerum veniam sed obcaecati alias, culpa enim non necessitatibus
+					laboriosam esse laudantium, tempore asperiores nesciunt! Totam, molestias.
+				</p>
 			</div>
 		</div>
 	);
