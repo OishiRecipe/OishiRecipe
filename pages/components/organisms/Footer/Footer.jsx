1d85868efcad09be70788162d5b795a3386a71ee
TolgaKara <tolgakara2.0@gmail.com>
2021-03-16 12:38:06 +0100

    Styled and Updated Footer

--- pages/components/organisms/Footer/Footer.jsx
+++ pages/components/organisms/Footer/Footer.jsx
@@ -13,11 +13,21 @@ export const Footer = () => {
 		<footer className={styles.footer}>
 			<div>
 				<div className={styles.socialLinks}>
-					<AiFillFacebook />
-					<AiOutlineInstagram />
-					<AiFillTwitterSquare />
-					<AiFillLinkedin />
-					<FaXingSquare />
+					<a href='https://www.facebook.com' target='__blank'>
+						<AiFillFacebook className={styles.link} />
+					</a>
+					<a href='https://www.instagram.com' target='__blank'>
+						<AiOutlineInstagram className={styles.link} />
+					</a>
+					<a href='https://www.twitter.com' target='__blank'>
+						<AiFillTwitterSquare className={styles.link} />
+					</a>
+					<a href='https://www.linkedin.com' target='__blank'>
+						<AiFillLinkedin className={styles.link} />
+					</a>
+					<a href='https://www.xing.com' target='__blank'>
+						<FaXingSquare className={styles.link} />
+					</a>
 				</div>
 				<p>© 2021 OishiRecipe. All rights reserved.</p>
 			</div>
